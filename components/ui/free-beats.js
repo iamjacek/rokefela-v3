4ebab7e0a2d8f25a78e16f4cafa02639c963f4b8
iamjacek <jake47s@gmail.com>
2025-02-15 16:51:12 +0000

    add styling

--- components/ui/free-beats.js
+++ components/ui/free-beats.js
@@ -63,16 +63,16 @@ export default function FreeBeats() {
 
   return (
     <>
-      <Container className="mx-auto mb-8 bg-rokefelaBlack">
+      <Container className="mx-auto mb-8 bg-rokefelaBlack md:mb-16 xl:mb-24">
         <div className="h-[92px]"></div>
         <article>
           <div>
-            <div className="mb-16 bg-brand bg-opacity-[45%] px-4 py-8 md:px-8">
-              <h1 className="z-0 w-full text-center font-serif text-3xl uppercase tracking-wider text-white lg:text-5xl 2xl:text-6xl">
+            <div className="mb-16 bg-brand bg-opacity-[45%] px-4 py-8 sm:px-8 xl:py-10">
+              <h1 className="z-0 w-full text-center font-serif text-3xl uppercase tracking-wider text-white md:text-6xl xl:text-7xl">
                 8 FREE FOR PROFIT BEATS
               </h1>
             </div>
-            <div className="color-white mx-auto my-8 max-w-5xl px-4 text-base md:px-8">
+            <div className="color-white mx-auto my-8 max-w-5xl px-4 text-base sm:px-8 lg:max-w-xl">
               <p className="pb-8 font-bold">
                 Perform, stream, sell, promote, drop album & EARN
                 MONEY on all digital platforms like: SPOTIFY, APPLE
@@ -93,30 +93,30 @@ export default function FreeBeats() {
               validationSchema={requiredSchema}
               onSubmit={handleSubmit}>
               <Form
-                className={`${isOpen ? "visible opacity-100" : "invisible opacity-0"} mx-4 box-border flex w-[calc(100%-30px)] items-center justify-center bg-rokefelaBlack text-center opacity-0 drop-shadow-lg transition-all md:mx-0 md:h-[500px] md:w-6/12 md:max-w-[500px] md:px-6`}>
-                <div>
-                  <p>Email Address</p>
+                className={`${isOpen ? "visible opacity-100" : "invisible opacity-0"} mx-4 box-border flex w-[calc(100%-32px)] items-center justify-center bg-rokefelaBlack text-center opacity-0 drop-shadow-lg transition-all sm:mx-8 sm:w-[calc(100%-64px)] lg:mx-auto lg:max-w-xl lg:px-8`}>
+                <div className="w-full">
+                  <p className="text-left">Email Address</p>
 
                   <Field
                     type="email"
                     name="email"
-                    className="relative mt-4 w-full grow border-2 bg-transparent bg-white px-5 py-3 text-rokefelaBlack outline-none outline-none placeholder:text-gray-700 focus:ring-4 md:mt-8"
-                    placeholder="E-mail address"
+                    className="relative mb-4 mt-1 w-full grow border-2 bg-transparent bg-white px-4 py-4 text-[18px] text-rokefelaBlack outline-none outline-none placeholder:text-gray-700 focus:ring-4"
+                    placeholder="james@mail.com"
                     autoComplete="off"
                   />
-                  <p>First Name</p>
+                  <p className="text-left">First Name</p>
                   <Field
                     type="text"
                     name="name"
-                    className="relative mt-8 w-full grow border-2 bg-transparent bg-white px-5 py-3 text-rokefelaBlack outline-none outline-none placeholder:text-gray-700 focus:ring-4 md:mt-12"
-                    placeholder="First Name"
+                    className="relative mb-4 mt-1 w-full grow border-2 bg-transparent bg-white px-4 py-4 text-[18px] text-rokefelaBlack outline-none outline-none placeholder:text-gray-700 focus:ring-4"
+                    placeholder="James"
                     autoComplete="off"
                   />
                   <button
-                    className="main-button lg: mx-auto mb-8 mt-2 mt-4 w-full border-2 border-white px-4 py-4 font-button text-base tracking-widest text-white transition-all sm:border-[3px] lg:px-2"
+                    className="main-button mx-auto mb-16 mt-0 w-full border-2 border-white px-4 py-4 font-button text-base tracking-widest text-white transition-all sm:border-[3px] lg:px-2"
                     type="submit"
                     disabled={buttonDisabled}>
-                    GET 8 FREE FOR PROFIT BEATS
+                    GET YOUR BEATS🎧
                   </button>
                   {message && (
                     <p
@@ -124,6 +124,9 @@ export default function FreeBeats() {
                       {message}
                     </p>
                   )}
+                  <p className="text-left text-sm">
+                    One-click unsubscribe option guaranteed.
+                  </p>
                 </div>
               </Form>
             </Formik>
